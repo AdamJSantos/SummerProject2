1f7d3a6ff4246c4c48b13155d4f782c155a2b740
AdamJSantos <asantos1116123@gmail.com>
2026-06-21 23:08:50 -0700

    Front End display fixes

--- client/recipe-gen/src/App.js
+++ client/recipe-gen/src/App.js
@@ -20,21 +20,21 @@ const RecipeCard = ({onSubmit}) => {
   };
 
   return (
-    <div className='w-[400px] border rounded-lg overflow-hidden shawdow-lg'>
-      <div className="p-6 py-4">
-      <div className="font-bold text-xl mb-2">Recipe Generator</div>
-        <div className="mb-4">
+    <div className= 'w-[400px] border rounded-lg overflow-hidden shawdow-lg'>
+      <div className= 'p-6 py-4'>
+      <div className='font-bold text-xl mb-2'>Recipe Generator</div>
+        <div className= 'mb-4'>
         <label
-          className="block text-gray-700 text-sm font-bold mb-2"
-          htmlFor="ingredients"
+          className='block text-gray-700 text-sm font-bold mb-2'
+          htmlFor='ingredients'
         >
           Ingredients:
         </label>
         <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="ingredients"
-          type="text"
-          placeholder="Enter ingredients"
+          className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+          id='ingredients'
+          type='text'
+          placeholder='Enter ingredients'
           value={ingredients}
           onChange={(e) => setIngredients(e.target.value)}
         />
@@ -76,13 +76,13 @@ const RecipeCard = ({onSubmit}) => {
       </div>
       <div className="mb-4">
         <label
-          className="block text-gray-700 text-sm font-bold mb-2"
-          htmlFor="cookingTime"
+          className= "block text-gray-700 text-sm font-bold mb-2"
+          htmlFor= "cookingTime"
         >
           Cooking Time:
         </label>
         <select
-          className="black appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+          className= "black appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
           id="cookingTime"
           value={cookingTime}
           onChange={(e) => setcookingTime(e.target.value)}
